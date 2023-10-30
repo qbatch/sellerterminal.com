88b7a6a29da56e9fce53a80a2d7e150b7d3d778b
imran-ali-qbatch <imran.ali@qbatch.com>
2023-10-30 18:04:41 +0500

    google tag managers added

--- src/layout/index.js
+++ src/layout/index.js
@@ -19,8 +19,27 @@ const Layout = ({ children }) => {
           href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap"
           rel="stylesheet"
         />
+        {/* Google Tag Manager */}
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-BNQQQD2B');`}
+        </script>
+        {/* End Google Tag Manager */}
       </Helmet>
       <GlobalStyle />
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-BNQQQD2B"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+      {/*  End Google Tag Manager (noscript) */}
       {children}
       <Footer />
     </ThemeProvider>
