a439efb96be058957b1c5f4440100eda6a0d3ade
imran-ali-qbatch <imran.ali@qbatch.com>
2023-10-31 10:05:57 +0500

    google site verification meta tag added on head

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -23,6 +23,10 @@ const SEO = ({ title, description, pathname, children }) => {
       <meta name="description" content={seo.description} />
       <link rel="canonical" href={seo.url} />
       <meta name="keywords" content={seo.keywords} />
+      <meta
+        name="google-site-verification"
+        content="opICxee3noY9Tt6UqmCqi-bSE4HaTxwIVwyuielRJSI"
+      />
       <meta http-equiv="content-language" content="en-us" />
       {children}
     </>
