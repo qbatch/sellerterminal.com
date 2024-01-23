b73104028b5d23068fc73b60abe3a1881e339b6e
ali-haider-qbatch <ali@qbatch.com>
2024-01-23 15:55:24 +0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1143e2a193bd14110c77.js"
+    "url": "webpack-runtime-2d6b37ca74a43bb338d5.js"
   },
   {
     "url": "framework-8747b46d847e77fe08f7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2fb61a8514148f61d5eb082feb933d46"
+    "revision": "3a6545869718fb3217b3e5b655235f93"
   },
   {
     "url": "manifest.webmanifest",
